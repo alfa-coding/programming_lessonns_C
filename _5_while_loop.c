2d75cae28b7dfabf6826500e3f07dfdb0ca32943
DimasVeliz <trevor90094@yandex.com>
2021-10-01 12:51:52 +0200

    While loop added

---
+++ _5_while_loop.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/*while loop:
+
+while(condition)
+{
+    //body that repeats under the condition being true
+}
+
+*/
+
+
+int main()
+{
+    int counter =0;
+
+    while (counter < 5)
+    {
+        /* code */
+        printf("Dimas\n");
+
+        counter = counter +1;
+    }
+    
+    
+    return 0;
+}
