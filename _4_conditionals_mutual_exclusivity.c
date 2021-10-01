daaba66089fbe1d5e2f0df9ba2dd3abc40920fdb
DimasVeliz <trevor90094@yandex.com>
2021-10-01 12:48:50 +0200

    renaming done to clarify order of lessons

---
+++ _4_conditionals_mutual_exclusivity.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int a =4545;
+    int b =20;
+
+    if(a >b)
+    {
+        printf("a es el mayor");
+    }
+    else if( b>a)
+    {
+        printf("b es el mayor");
+    }
+    else
+    {
+        printf("son iguales");
+    }
+
+
+    int k =7;
+
+    return 0;
+}
