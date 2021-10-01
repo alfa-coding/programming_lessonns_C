4c573da8eb2e25d65206ba5c53a69a45ede0e7c8
DimasVeliz <trevor90094@yandex.com>
2021-10-01 12:35:16 +0200

    Variable types explained

---
+++ variable_types.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+    the int type can storage
+    integer numbers in a range
+    that depends on the architecture
+    */
+    int numA = 5;
+    int numB =-14524;
+
+
+    /*
+    the float type can storage
+    floating point numbers in a range
+    that depends on the architecture
+    */
+    float numC = 5.4f;
+    float numD =-3.7f;
+
+    /*
+    the char type can storage
+    ASCII characters*/
+    char letterA = 'A';
+    char letterB ='f';
+
+    return 0;
+}
